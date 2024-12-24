970f41c5556784a39a559a94ac6c27e01c087d80
San Vuthdimorng <56204764+Dimong-001@users.noreply.github.com>
2024-12-24 16:17:56 +0700

    add some more style

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   const decorations = document.querySelector(".decorations");
+  const snowContainer = document.querySelector(".snow-container");
 
   // Generate ornaments dynamically
-  for (let i = 0; i < 40; i++) {
+  for (let i = 0; i < 60; i++) {
     const ornament = document.createElement("div");
     if (i % 2 === 0) {
       ornament.className = "bauble";
@@ -12,8 +13,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Randomize position on the tree
     ornament.style.top = `${Math.random() * 600}px`;
-    ornament.style.left = `${Math.random() * 350}px`;
+    ornament.style.left = `${Math.random() * 450}px`;
 
     decorations.appendChild(ornament);
+  }
+
+  // Generate snowflakes
+  for (let i = 100; i > 0; i--) {
+    const snowflake = document.createElement("div");
+    snowflake.className = "snowflake";
+    snowflake.style.left = `${Math.random() * 100}%`;
+    snowflake.style.width = `${Math.random() * 5 + 5}px`;
+    snowflake.style.height = snowflake.style.width;
+    snowflake.style.animationDuration = `${Math.random() * 3 + 7}s`;
+    snowContainer.appendChild(snowflake);
   }
 });
